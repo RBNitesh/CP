f43d6f3b6c0508a3040bd59a1f0a34b49e3993ad
Nitesh Kumar <nk41521391@gmail.com>
2026-05-02 21:18:37 +0530

    ThousandRated Problem

--- C++/ThousandRated/Raspbarries.cpp
+++ C++/ThousandRated/Raspbarries.cpp
@@ -1,16 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int compute(int m, set<int> set, int k){
-    if(m == 0)
-        return 0;
-    int ans = INT_MAX;
-    for(int it : set){
-        ans = min(ans, 1 + compute((m * it) % k, set, k));
-    }
-    return ans;
-}
-
 int main(){
     int t;
     cin >> t;
@@ -32,27 +22,46 @@ int main(){
             m %= k;
         }
 
-        vector<int> preMult;
-        vector<int> postMult;
+        vector<int> preMult(n, 0);
+        vector<int> postMult(n, 0);
 
-        preMult.push_back(vec[0]%k);
+        preMult[0] = vec[0]%k;
         for (int i = 1; i < vec.size(); i++){
-            preMult.push_back((preMult[i - 1] * vec[i]) % k);
+            preMult[i] = (preMult[i - 1] * vec[i]) % k;
         }
 
-        postMult.push_back(vec[n - 1] % k);
+        postMult[n-1] = vec[n - 1] % k;
         for (int i = n - 2; i >= 0; i--){
-            postMult.push_back((postMult[i + 1] * vec[i]) % k);
+            postMult[i] = (postMult[i + 1] * vec[i]) % k;
         }
 
-        set<int> set;
+        unordered_set<int> u_set;
         for (int i = 0; i < n; i++){
-            set.emplace(((i == 0? 1 : preMult[i-1]) * (i == n-1? 1 : postMult[i+1])) % k);
+            int rem = (i == 0? 1 : preMult[i-1]) * (i == n-1? 1 : postMult[i+1]) % k;
+            u_set.emplace(rem);
         }
 
-        int res = compute(m, set, k);
+        unordered_set<int> found;
+        found.emplace(m);
 
-        cout << res << "\n";
+        int ops = 0;
+        while(!found.count(0)){
+            unordered_set<int> new_found;
+
+            for(int it1 : found){
+                for(int it2 : u_set){
+                    new_found.emplace((it1 + it2) % k);
+                }
+            }
+
+            for(int it : new_found){
+                found.emplace(it);
+            }
+
+            ops++;
+        }
+
+        cout << ops << "\n";
     }
     return 0;
 }
